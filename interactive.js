2e697d5f463c04a20aea277fc1ad7dadd777979f
sameulyung2025 <113876575+sameulyung2025@users.noreply.github.com>
2022-11-14 00:10:14 +0800

    Add files via upload

---
+++ interactive.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("button").onclick = () => {
+        document.querySelector("button").style.backgroundColor = hsl(25, 97, 53);
+    }
+});
